1fbb3ac4ab15369e76a02dba8a308063cc16da72
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2026-02-16 13:10:06 +0330

    feat: implement character API endpoints for fetching all characters and individual character by ID

--- server/api/character/index.get.ts
+++ server/api/character/index.get.ts
@@ -1,5 +1,9 @@
-export default defineEventHandler(async () => {
-  const { apiBaseUrl } = useRuntimeConfig();
-  return $fetch(`${apiBaseUrl}/posts`);
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const query = getQuery(event);
+
+  return $fetch(`${config.public.apiBaseUrl}/character`, {
+    query,
+  });
 });
 
