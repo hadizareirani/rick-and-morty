fd1a61f8874a8b81f5a7c0f1fef1dd5b1b6ceca6
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2026-02-16 13:15:27 +0330

    feat: display fetched character data in the home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,19 @@
+<script setup lang="ts">
+const { fetchAll } = useCharacter();
+const { data: characters, pending } = await fetchAll();
+</script>
 <template>
   <div>
     <AppHeader>
       <SearchBar />
     </AppHeader>
+
+    <div>
+      <pre>
+      <code>
+         {{ JSON.stringify(characters, null, 2) }}
+      </code>
+     </pre>
+    </div>
   </div>
 </template>
-
